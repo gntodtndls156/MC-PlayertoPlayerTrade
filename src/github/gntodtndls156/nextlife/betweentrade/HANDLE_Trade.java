04fa1412e4208367b72dec796a5fb8e3acf9fb17
gntodtndls156 <gntodtndls156@gmail.com>
2020-12-30 19:05:24 +0900

    add button

--- src/github/gntodtndls156/nextlife/betweentrade/HANDLE_Trade.java
+++ src/github/gntodtndls156/nextlife/betweentrade/HANDLE_Trade.java
@@ -1,12 +1,16 @@
 package github.gntodtndls156.nextlife.betweentrade;
 
 import org.bukkit.Bukkit;
+import org.bukkit.DyeColor;
 import org.bukkit.Material;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.material.Wool;
 
 import java.util.Arrays;
 import java.util.HashMap;
@@ -17,6 +21,9 @@ public class HANDLE_Trade {
     static Map<String, INV_Trade> TRADE = new HashMap<String, INV_Trade>();
     final int[] playerMeSlot = new int[]{0, 1, 2, 3, 9, 10, 11, 12, 18, 19, 20, 21, 27, 28, 29, 30};
     final int[] playerYouSlot = new int[]{5, 6, 7, 8, 14, 15, 16, 17, 23, 24, 25, 26, 32, 23, 24, 35};
+    final int[] lineLeft = new int[]{36, 37, 38};
+    final int[] lineRight = new int[]{44, 43, 42};
+    final int[] lineCenter = new int[]{40, 31, 22, 13, 4};
     String TRADE_key;
 
     public HANDLE_Trade(String playerMe, String playerYou) {
@@ -24,17 +31,19 @@ public class HANDLE_Trade {
         TRADE.put(TRADE_key, new INV_Trade(Bukkit.getPlayer(playerMe), Bukkit.getPlayer(playerYou)));
         TRADE.get(TRADE_key).openInventory();
     }
+    BetweenTradeMain plugin
+    public HANDLE_Trade(BetweenTradeMain plugin) {
+        this.plugin = plugin;
+    }
+
 
     @EventHandler
     public void onClickAtTradeInventory(InventoryClickEvent event) {
         if (event.getInventory().getTitle().equals("Player To Player Trade")) {
             event.setCancelled(true);
-            Player player;
-            String player$name = null;
-            if (event.getWhoClicked() instanceof Player) {
-                player = (Player) event.getWhoClicked();
-                player$name = player.getName();
-            }
+
+            final Player player = (Player) event.getWhoClicked();
+            final String player$name = player.getName();
 
             if (event.getRawSlot() == 53) {
                 ItemStack Button = event.getCurrentItem();
@@ -42,6 +51,25 @@ public class HANDLE_Trade {
 
                 if (Button$name.equals("거래 잠그기")) {
                     // TODO
+                    for(int i = 0; i < 3; i++) {
+                        int finalI = i;
+                        Bukkit.getScheduler().runTaskLater(plugin, () -> {
+                            TRADE.get(TRADE_key).getInventory().setItem(
+                                    TRADE.get(TRADE_key).getPlayerMe().getName().equals(player$name) ? lineLeft[finalI] : lineRight[finalI],
+                                    new ItemStack(Material.STAINED_GLASS_PANE, 1, (short) 12)
+                            );
+                            if (finalI == 3) {
+                                if (TRADE.get(TRADE_key).getPlayerMe().getName().equals(player$name)) {
+                                    TRADE.get(TRADE_key).setMeLock(true);
+                                } else {
+                                    TRADE.get(TRADE_key).setYouLock(true);
+                                }
+                                if (TRADE.get(TRADE_key).isMeLock() && TRADE.get(TRADE_key).isYouLock()) {
+
+                                }
+                            }
+                        }, (1 + i) * 7L);
+                    }
                 } else if (Button$name.equals("거래 수락하기")) {
                     // TODO
                 } else if (Button$name.equals("돈 거래")) {
@@ -74,7 +102,25 @@ public class HANDLE_Trade {
                     }
             }
         }
-
-        
     }
+    private void changeLine(int color, InventoryClickEvent event, boolean state) {
+        for(int i = 0; i < lineCenter.length; i++) {
+            int finalI = i;
+            Bukkit.getScheduler().runTaskLater(plugin, () -> {
+                TRADE.get(TRADE_key).getInventory().setItem(
+                        lineCenter[finalI],
+                        new ItemStack(Material.STAINED_GLASS_PANE, 1, (short) color)
+                );
+                if (finalI + 1 == lineCenter.length) {
+                    if (state) {
+                        TRADE.get(TRADE_key).getInventory().setItem(53, new INIT_Button(2).ButtonLock());
+                    } else {
+
+                    }
+                }
+            }, (i + 1) * 7);
+        }
+    }
+
+    
 }
---
+++ src/github/gntodtndls156/nextlife/betweentrade/INIT_Button.java
@@ -0,0 +1,51 @@
+package github.gntodtndls156.nextlife.betweentrade;
+
+import org.bukkit.DyeColor;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.material.Wool;
+
+import java.util.Arrays;
+
+public class INIT_Button {
+    int number;
+    public INIT_Button(int number) {
+        this.number = number;
+    }
+
+    public ItemStack ButtonLock() {
+        switch (number) {
+            case 1:
+                return ButtonLock("거래 잠그기", new String[] {"이 재화로 거래하자고 제안합니다."}, 14);
+            case 2:
+                return ButtonLock("거래 수락하기", new String[]{"최종적으로 거래를 수락합니다.", "상대방도 수락할 경우 거래가 완료됩니다."}, 11);
+        }
+        return null;
+    }
+    private ItemStack ButtonLock(String name, String[] lore, int color) {
+        ItemStack lock = new ItemStack(WoolColors(color));
+        ItemMeta meta = lock.getItemMeta();
+
+        meta.setDisplayName(name);
+        meta.setLore(Arrays.asList(lore));
+        lock.setItemMeta(meta);
+        return lock;
+    }
+    private ItemStack WoolColors(int number) {
+        DyeColor[] colors = DyeColor.values();
+        return new Wool(colors[number]).toItemStack(1);
+        //Wool wool = new Wool(colors[number]);
+    }
+
+    private ItemStack ButtonMoney() {
+        ItemStack item = new ItemStack(Material.GOLD_NUGGET, 1);
+        ItemMeta meta = item.getItemMeta();
+
+        meta.setDisplayName("돈 거래");
+        meta.setLore(Arrays.asList("돈을 얼만큼 주고 받을지 요구합니다."));
+
+        item.setItemMeta(meta);
+        return item;
+    }
+}
